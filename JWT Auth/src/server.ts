b8f0ed754e2ca7cab27984df0be1fc0f59cad612
ivandoza <ivandoza@gmail.com>
2024-03-08 19:48:29 +0000

    JWT exercises

---
+++ JWT Auth/src/server.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+  createImage,
+} from "./controllers/planets.js";
+import multer from "multer";
+import authorize from "./authorize.js";
+import { logIn, signUp, logOut } from "./controllers/users.js";
+import "./passport.js"
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/api/planets", getAll);
+app.get("/api/planets/:id", getOneById);
+app.post("/api/planets", create);
+app.put("/api/planets/:id", updateById);
+app.delete("/api/planets/:id", deleteById);
+
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+
+app.post("/api/users/login", logIn);
+app.post("/api/users/signup", signUp);
+app.post("/api/users/logout", authorize, logOut);
+
+
+app.listen(port, () => {
+  console.log(`App listening on port http:/localhost:${port}`);
+});
